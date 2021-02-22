f24979ddddae872f6064c4ee9a5650a77f0823c5
Artur Petryk <petrikartur@gmail.com>
2021-02-22 15:23:45 +0200

    updated urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from register import views as v
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', v.register, name='register'),
     path('', include('main.urls')),
     path('', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
